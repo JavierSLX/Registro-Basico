c32c27950bb698960614c12f78a305e991841d3c
JavierSLX <jserranolule@gmail.com>
2020-01-29 16:39:50 -0600

    Colocando imagenes

--- lib/src/pages/PhotoPage.dart
+++ lib/src/pages/PhotoPage.dart
@@ -10,13 +10,23 @@ class _PhotoPageState extends State<PhotoPage> {
   @override
   Widget build(BuildContext context) {
 
-    CardDart card = new CardDart();
+    List<int> imagenes = [1, 2, 3, 4, 5];
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Album de fotos"),
         centerTitle: true,
       ),
+      body: ListView.builder(
+        itemCount: imagenes.length,
+        itemBuilder: (BuildContext context, int index){
+          //Regresa una imagen sacada de internet
+          return FadeInImage(
+            placeholder: AssetImage("assets/loading_2.gif"),
+            image: NetworkImage("https://picsum.photos/500/300/?image=${imagenes[index]}"),
+          );
+        },
+      )
     );
   }
 }
